749f19c194d9b356ed102a816e6e04de4f1198bf
TomReilly1 <reilly.thomas.c@gmail.com>
2022-07-20 22:10:54 -0400

    add vue router

---
+++ vite-project/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/HomeView.vue')
+  },
+  {
+    path: '/kanban',
+    name: 'Kanban',
+    component: () => import('../views/KanbanView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+
+export default router
