80bd39a258f089f36d0276087f0a972d4d37faa3
TomReilly1 <reilly.thomas.c@gmail.com>
2022-07-25 18:38:07 -0400

    finalize ts conversion

---
+++ project-ts/src/components/TheHeader.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+import { ref, watch } from "vue";
+import { useRoute } from 'vue-router'
+
+
+const route = useRoute()
+const header = ref<string>('')
+
+
+function capitalizeWord(header: string) {
+    const lower: string = header.toLowerCase()
+    const first: string = lower.charAt(0).toUpperCase()
+    const output: string = first + lower.slice(1)
+    return output
+}
+
+
+watch(route, () => {
+    const routeName: string = <string>route.name
+
+    if (!routeName) {
+        console.error('Page not found!')
+    } else {
+        header.value = routeName
+        console.log(typeof routeName)
+    }
+})
+</script>
+<!------------------------------------------------->
+<template>
+    <header>
+        <div>
+            <h1>{{header}}</h1>
+        </div>
+    </header>
+</template>
+<!------------------------------------------------->
+<style scoped>
+header {
+    width: 100%;
+    height: 100px;
+    cursor: default;
+    /* margin: 0 0 30px; */
+}
+
+div {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+h1 {
+    margin: 0;
+    font-size: 2.2rem;
+    text-decoration: underline;
+}
+</style>
---
+++ project-ts/src/components/TheNavbar.vue
@@ -0,0 +1,77 @@
+<script lang="ts" setup>
+
+</script>
+<!------------------------------------------------->
+<template>
+    <nav>
+        <ul>
+            <li>
+                <h2>
+                    <router-link to="/">Tom's Portfolio</router-link>
+                </h2>
+            </li>
+            <li>
+                <router-link :to="{ name: 'Kanban'}">
+                    Kanban
+                </router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'Calculator'}">
+                    Calculator
+                </router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+<!------------------------------------------------->
+<style scoped>
+h2 {
+    margin: 0;
+    padding: 0;
+    font-size: 1rem;
+}
+
+nav {
+    /* height: 100vh; */
+    min-height: 100vh;
+    width: 150px;
+    min-width: 150px;
+    background-color: #4a4a4a;
+}
+
+ul {
+    margin: 0;
+    padding: 10px 0px;
+}
+
+li {
+    display: flex;
+    justify-content: center;
+    margin: 15px 0;
+    padding-left: 15px;
+}
+
+li:first-child {
+    border-bottom: 2px solid rgba(0,0,0,0.2);
+    padding-bottom: 15px;
+    color: var(--gold1);
+}
+
+li > * {
+    color: unset;
+    display: inline-block;
+    z-index: 1;
+    width: 100%;
+    /* text-decoration: none; */
+    /* transition: color 0.3s; */
+}
+
+/* li > *:visited {
+    color: unset;
+}
+
+li > *:hover {
+    color: var(--purple1);
+} */
+
+</style>
