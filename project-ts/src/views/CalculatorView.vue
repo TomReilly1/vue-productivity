30def3c7eb4a37791a33baf835c45c7c945bd4ab
TomReilly1 <reilly.thomas.c@gmail.com>
2022-07-25 18:35:00 -0400

    add views

---
+++ project-ts/src/views/CalculatorView.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { ref, reactive } from 'vue'
+import { emit } from 'process';
+import Calculator from "../components/Calculator.vue";
+
+
+
+</script>
+<!------------------------------------------------->
+<template>
+<main>
+    <Calculator />
+</main>
+</template>
+<!------------------------------------------------->
+<style scoped>
+main {
+    min-height: calc(100% - 200px);
+}
+</style>
---
+++ project-ts/src/views/KanbanView.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import KanbanForm from '../components/KanbanForm.vue'
+import KanbanBoard from '../components/KanbanBoard.vue'
+
+
+interface taskIntr {
+    proj: string,
+    desc: string,
+    sts: string
+}
+
+const newTaskRef = ref<taskIntr>({
+    proj: '',
+    desc: '',
+    sts: ''
+})
+
+
+function addTaskCard(emitObj: taskIntr) {
+    console.log(emitObj)
+    console.log(newTaskRef.value)
+
+    newTaskRef.value = emitObj
+
+    console.log(newTaskRef.value)
+
+}
+</script>
+<!------------------------------------------------->
+<template>
+<main>
+    <KanbanForm @add-task="addTaskCard"/>
+    <KanbanBoard :new-task="newTaskRef"/>
+</main>
+</template>
+<!------------------------------------------------->
+<style scoped>
+main {
+    min-height: calc(100% - 200px);
+}
+</style>
